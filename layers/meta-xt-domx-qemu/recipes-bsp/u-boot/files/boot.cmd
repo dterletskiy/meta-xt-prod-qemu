5963cac731b464fa9c7bfd30945e3b0846347ef6
Dmitriy Terletskiy <daterletskiy@gmail.com>
2024-12-19 09:24:21 +0000

    update
    Signed-off-by: Dmitriy Terletskiy <daterletskiy@gmail.com>

--- layers/meta-xt-domx-qemu/recipes-bsp/u-boot/files/boot.cmd
+++ layers/meta-xt-domx-qemu/recipes-bsp/u-boot/files/boot.cmd
@@ -1,3 +1,47 @@
-setenv bootargs console=ttyAMA0,115200 root=/dev/mmcblk0p2 rw
-load mmc 0:1 0x80000000 /boot/uImage
-bootm 0x80000000
+setenv XEN_ADDRESS         0x50000000
+setenv XEN_SIZE            0x0
+setenv DTB_ADDRESS         0x51000000
+setenv KERNEL_ADDRESS      0x60000000
+setenv KERNEL_SIZE         0x0
+setenv ROOTFS_ADDRESS      0x52000000
+setenv ROOTFS_SIZE         0x0
+
+
+
+setenv bootargs
+
+size virtio 0:1 xen
+setenv XEN_SIZE ${filesize}
+ext4load virtio 0:1 ${XEN_ADDRESS} xen
+
+size virtio 0:1 kernel-dom0
+setenv KERNEL_SIZE ${filesize}
+ext4load virtio 0:1 ${KERNEL_ADDRESS} kernel-dom0
+
+size virtio 0:1 rootfs-dom0
+setenv ROOTFS_SIZE ${filesize}
+ext4load virtio 0:1 ${ROOTFS_ADDRESS} rootfs-dom0
+
+fdt addr -c
+setenv DTB_ADDRESS ${fdtcontroladdr}
+
+fdt addr ${DTB_ADDRESS}
+
+fdt list /chosen
+
+fdt set /chosen \#address-cells <1>
+fdt set /chosen \#size-cells <1>
+fdt set /chosen bootargs
+
+fdt resize
+fdt mknod /chosen module@0
+fdt set /chosen/module@0 compatible "xen,linux-zimage" "xen,multiboot-module"
+fdt set /chosen/module@0 reg <${KERNEL_ADDRESS} ${KERNEL_SIZE}>
+fdt set /chosen/module@0 bootargs "root=/dev/ram verbose loglevel=7 console=hvc0 earlyprintk=xen"
+
+fdt resize
+fdt mknod /chosen module@1
+fdt set /chosen/module@1 compatible "xen,linux-initrd" "xen,multiboot-module"
+fdt set /chosen/module@1 reg <${ROOTFS_ADDRESS} ${ROOTFS_SIZE}>
+
+setenv boot_xen "booti ${XEN_ADDRESS} - ${XEN_ADDRESS}"
