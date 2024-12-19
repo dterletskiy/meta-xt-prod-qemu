29c79e3beb23454de811f772cd52f81631940602
Dmitriy Terletskiy <daterletskiy@gmail.com>
2024-12-19 12:22:10 +0000

    update
    Signed-off-by: Dmitriy Terletskiy <daterletskiy@gmail.com>

--- layers/meta-xt-domx-qemu/recipes-bsp/u-boot/files/boot.cmd
+++ layers/meta-xt-domx-qemu/recipes-bsp/u-boot/files/boot.cmd
@@ -1,12 +1,36 @@
 setenv XEN_ADDRESS         0x50000000
 setenv XEN_SIZE            0x0
-setenv XEN_CMDLINE			"dom0_mem=3G,max:3G loglvl=all guest_loglvl=all console=dtuart"
+setenv XEN_CMDLINE         "dom0_mem=3G,max:3G loglvl=all guest_loglvl=all console=dtuart"
 setenv DTB_ADDRESS         0x51000000
 setenv KERNEL_ADDRESS      0x60000000
 setenv KERNEL_SIZE         0x0
-setenv KERNEL_CMDLINE 		"root=/dev/ram verbose loglevel=7 console=hvc0 earlyprintk=xen"
+setenv KERNEL_CMDLINE      "root=/dev/ram verbose loglevel=7 console=hvc0 earlyprintk=xen"
 setenv ROOTFS_ADDRESS      0x52000000
 setenv ROOTFS_SIZE         0x0
+
+
+
+setenv patch_dtb_chosen '
+   fdt resize
+   fdt set /chosen \#address-cells <1>
+   fdt set /chosen \#size-cells <1>
+   fdt set /chosen bootargs "${XEN_CMDLINE}"
+
+   fdt resize
+   fdt mknod /chosen module@0
+   fdt set /chosen/module@0 compatible "xen,linux-zimage" "xen,multiboot-module"
+   fdt set /chosen/module@0 reg <${KERNEL_ADDRESS} ${KERNEL_SIZE}>
+   fdt set /chosen/module@0 bootargs "${KERNEL_CMDLINE}"
+
+   fdt resize
+   fdt mknod /chosen module@1
+   fdt set /chosen/module@1 compatible "xen,linux-initrd" "xen,multiboot-module"
+   fdt set /chosen/module@1 reg <${ROOTFS_ADDRESS} ${ROOTFS_SIZE}>
+'
+
+setenv boot_xen '
+   booti ${XEN_ADDRESS} - ${DTB_ADDRESS}
+'
 
 
 
@@ -29,23 +53,10 @@ setenv DTB_ADDRESS ${fdtcontroladdr}
 
 fdt addr ${DTB_ADDRESS}
 
-fdt set /chosen \#address-cells <1>
-fdt set /chosen \#size-cells <1>
-fdt set /chosen bootargs "${XEN_CMDLINE}"
 
-fdt resize
-fdt mknod /chosen module@0
-fdt set /chosen/module@0 compatible "xen,linux-zimage" "xen,multiboot-module"
-fdt set /chosen/module@0 reg <${KERNEL_ADDRESS} ${KERNEL_SIZE}>
-fdt set /chosen/module@0 bootargs "${KERNEL_CMDLINE}"
 
-fdt resize
-fdt mknod /chosen module@1
-fdt set /chosen/module@1 compatible "xen,linux-initrd" "xen,multiboot-module"
-fdt set /chosen/module@1 reg <${ROOTFS_ADDRESS} ${ROOTFS_SIZE}>
+
 
 fdt list /chosen
 fdt list /chosen/module@0
 fdt list /chosen/module@1
-
-setenv boot_xen "booti ${XEN_ADDRESS} - ${DTB_ADDRESS}"
