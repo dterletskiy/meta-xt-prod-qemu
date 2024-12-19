cbaf70cc637fd2980d459aed2b2dfd6ddb788a08
Dmitriy Terletskiy <daterletskiy@gmail.com>
2024-12-19 09:18:40 +0000

    update
    Signed-off-by: Dmitriy Terletskiy <daterletskiy@gmail.com>

--- layers/meta-xt-domx-qemu/recipes-bsp/u-boot/files/boot.cmd
+++ layers/meta-xt-domx-qemu/recipes-bsp/u-boot/files/boot.cmd
@@ -1,10 +1,1 @@
-setenv XEN_ADDRESS         0x50000000
-setenv XEN_SIZE            0x0
-setenv DTB_ADDRESS         0x51000000
-setenv KERNEL_ADDRESS      0x60000000
-setenv KERNEL_SIZE         0x0
-setenv ROOTFS_ADDRESS      0x52000000
-setenv ROOTFS_SIZE         0x0
-
-
-
+setenv bootargs
